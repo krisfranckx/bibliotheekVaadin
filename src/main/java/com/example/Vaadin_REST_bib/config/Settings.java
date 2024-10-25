b77fede9d276142af929ccd216a8ad3b6d570d95
krisfranckx <krisfranckx@hotmail.com>
2024-10-25 22:56:16 +0200

    add Backend url

--- src/main/java/com/example/Vaadin_REST_bib/config/Settings.java
+++ src/main/java/com/example/Vaadin_REST_bib/config/Settings.java
@@ -2,7 +2,7 @@ package com.example.Vaadin_REST_bib.config;
 
 public class Settings {
 
-    public static final String BASE_URL_BACK="http://localhost:3000";
+    public static final String BASE_URL_BACK="https://bibrest-production.up.railway.app";
     public static final String BASE_URL_FRONT="http://localhost:5000";
 
     //public static final String BASE_URL_BACK= "http://bibliotheek-atheneumboom-rest.eu-west-3.elasticbeanstalk.com";
